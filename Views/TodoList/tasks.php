8cb143d6ca83e830b65ac02dca2f74872f5d1cee
Julien Picard <julien.picard@coding-academy.fr>
2018-04-03 18:53:28 +0200

    views and vendors

--- Views/TodoList/tasks.php
+++ Views/TodoList/tasks.php
@@ -1,3 +1,16 @@
 <?php
+include_once (dirname(__FILE__) . '/../../Controllers/TodoList/tasksController.php');
+class Display {
 
+	public function displayTasks(){
+		$display = new TaskController;
+		$tasksToDisplay = $display->CheckTasks();
+		foreach ($tasksToDisplay as $key) {
+			echo $key['title'];
+			echo "<br/>";
+			echo $key['description'];
+			echo "<br/>";
+		}
+	}
+}
 ?>
