24f5a185556f830a8d6b2f3fe2aad784c59d06b2
jordane <jordane.agier@coding-academy.fr>
2018-04-03 18:56:24 +0200

    added controller

--- Controllers/TodoList/tasksController.php
+++ Controllers/TodoList/tasksController.php
@@ -1,4 +1,33 @@
 <?php 
 
+include_once (dirname(__FILE__) . '/../../Models/TodoList/Task.php');
 
-?>
+class TaskController
+{
+    function check_format ($title, $description = null) {
+        if (isset($title) && $title != "") {
+            if ($description) {
+                $description = $this->secure_input($description);
+                $title = $this->secure_input($title);
+                $task = new Task();
+                $task->post_task($title, $description);
+            } else {
+                $title = $this->secure_input($title);
+                $task = new Task();
+                $task->post_task($title);
+            }
+        } else {
+            echo "Please enter a title\n";
+        }
+    }
+
+    function secure_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+}
+
+$check = new TaskController();
+$check->check_format(" jopijo");
